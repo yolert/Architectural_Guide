db3c9c534dc5d7b72b73a12c410ba96b68daeaa9
veisdi <505169@niuitmo.ru>
2026-04-19 17:09:35 +0000

    add map

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//загрузки API Яндекс.Карт
+ymaps.ready(init);
 
+function init() {
+    //карта
+    var myMap = new ymaps.Map('map', {
+        center: [59.934280, 30.322760], // широта, долгота центра СПб
+        zoom: 14, // уровень приближения
+        controls: ['zoomControl', 'fullscreenControl'] // элементы управления
+    });
+
+    //маркер в центре
+    var myPlacemark = new ymaps.Placemark([59.934280, 30.322760], {
+        hintContent: 'Центр Санкт-Петербурга',
+        balloonContent: 'Здесь начинается наш архитектурный маршрут!'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
